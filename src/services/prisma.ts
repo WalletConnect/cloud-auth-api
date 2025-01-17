f558217d1f85e2de2660c652cf5e12bda5f6d23c
Cali93 <32299095+Cali93@users.noreply.github.com>
2025-01-17 22:46:54 +0700

    chore(sign-up): disable sign ups

--- src/services/prisma.ts
+++ src/services/prisma.ts
@@ -2,6 +2,7 @@ import { PrismaClient, users } from '@prisma/client'
 import jwt from 'jsonwebtoken'
 import { SiweMessage } from 'siwe'
 import { DEFAULT_INSTANCE_ID, SEVEN_DAYS_IN_SECONDS, secureToken } from '../utils'
+import { SiweDeprecationError } from '../utils/errors'
 import { GoTrueClaims, TPrismaTransaction } from '../utils/types'
 
 const prisma = new PrismaClient()
@@ -65,120 +66,73 @@ export async function createOrUpdateUser(siweMsg: SiweMessage) {
       }
     })
 
-    if (existingUser) {
-      // Check for an existing session and update the last sign in date
-      let existingSession = await tx.sessions.findFirst({
-        where: {
-          user_id: existingUser.id
-        }
-      })
-      await tx.users.update({
-        where: {
-          id: existingUser.id
-        },
-        data: {
-          last_sign_in_at: siweMsg.issuedAt
-        }
-      })
-      if (!existingSession) {
-        existingSession = await tx.sessions.create({
-          data: {
-            user_id: existingUser.id
-          }
-        })
-      }
-
-      let existingIdentity = await tx.identities.findFirst({
-        where: {
-          user_id: existingUser.id
-        }
-      })
-      if (!existingIdentity) {
-        await tx.identities.create({
-          data: {
-            id: existingUser.id,
-            provider: 'eth',
-            user_id: existingUser.id,
-            provider_id: siweMsg.address,
-            identity_data: {
-              sub: existingUser.id,
-              address: siweMsg.address
-            },
-            last_sign_in_at: siweMsg.issuedAt
-          }
-        })
-      }
-
-      // Generate access and refresh tokens
-      const generatedRefreshToken = secureToken()
-
-      const refreshToken = await tx.refresh_tokens.create({
-        data: {
-          session_id: existingSession.id,
-          user_id: existingUser.id,
-          instance_id: DEFAULT_INSTANCE_ID,
-          parent: '',
-          token: generatedRefreshToken
-        }
-      })
-      const accessToken = await generateAccessToken(
-        existingUser,
-        SEVEN_DAYS_IN_SECONDS,
-        tx,
-        existingSession.id
+    if (!existingUser) {
+      throw new SiweDeprecationError(
+        'Wallet account sign-ups are deprecated. Please sign up with your email and password.'
       )
-
-      return { refreshToken: refreshToken.token, accessToken }
     }
 
-    const newUser = await tx.users.create({
-      data: {
-        aud: 'authenticated',
-        role: 'authenticated',
-        email_confirmed_at: new Date().toISOString(),
-        instance_id: DEFAULT_INSTANCE_ID,
-        last_sign_in_at: siweMsg.issuedAt,
-        raw_app_meta_data: { provider: 'eth', providers: ['eth'] },
-        raw_user_meta_data: {
-          address: siweMsg.address,
-          chain_id: siweMsg.chainId.toString(),
-          namespace: 'eip155'
-        }
+    // Check for an existing session and update the last sign in date
+    let existingSession = await tx.sessions.findFirst({
+      where: {
+        user_id: existingUser.id
       }
     })
-
-    // Create the session and identity
-    const session = await tx.sessions.create({
+    await tx.users.update({
+      where: {
+        id: existingUser.id
+      },
       data: {
-        user_id: newUser.id
-      }
-    })
-    await tx.identities.create({
-      data: {
-        id: newUser.id,
-        provider: 'eth',
-        user_id: newUser.id,
-        provider_id: siweMsg.address,
-        identity_data: {
-          sub: newUser.id,
-          address: siweMsg.address
-        },
         last_sign_in_at: siweMsg.issuedAt
       }
     })
+    if (!existingSession) {
+      existingSession = await tx.sessions.create({
+        data: {
+          user_id: existingUser.id
+        }
+      })
+    }
 
-    // Generate tokens
+    let existingIdentity = await tx.identities.findFirst({
+      where: {
+        user_id: existingUser.id
+      }
+    })
+    if (!existingIdentity) {
+      await tx.identities.create({
+        data: {
+          id: existingUser.id,
+          provider: 'eth',
+          user_id: existingUser.id,
+          provider_id: siweMsg.address,
+          identity_data: {
+            sub: existingUser.id,
+            address: siweMsg.address
+          },
+          last_sign_in_at: siweMsg.issuedAt
+        }
+      })
+    }
+
+    // Generate access and refresh tokens
     const generatedRefreshToken = secureToken()
+
     const refreshToken = await tx.refresh_tokens.create({
       data: {
-        session_id: session.id,
-        user_id: newUser.id,
+        session_id: existingSession.id,
+        user_id: existingUser.id,
         instance_id: DEFAULT_INSTANCE_ID,
         parent: '',
         token: generatedRefreshToken
       }
     })
-    const accessToken = await generateAccessToken(newUser, SEVEN_DAYS_IN_SECONDS, tx, session.id)
+    const accessToken = await generateAccessToken(
+      existingUser,
+      SEVEN_DAYS_IN_SECONDS,
+      tx,
+      existingSession.id
+    )
 
     return { refreshToken: refreshToken.token, accessToken }
   })
